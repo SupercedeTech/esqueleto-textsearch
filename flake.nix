cc0cca6fae8e1fe79b072cbdc00b19b060ccf025
Jappie Klooster <jappieklooster@hotmail.com>
2023-08-02 09:09:59 -0400

    add flake

---
+++ flake.nix
@@ -0,0 +1,37 @@
+# I used chatgpt to generate this template and then just
+# modified to how I normally use these things.
+{
+  description = "My Haskell project";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-compat }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      hpkgs = pkgs.haskell.packages.ghc944.override {
+        overrides = hnew: hold: {
+          esqueleto-textsearch = hnew.callCabal2nix "esqueleto-textsearch" ./. { };
+        };
+      };
+    in
+    {
+      defaultPackage.x86_64-linux =  hpkgs.esqueleto-textsearch;
+      inherit pkgs;
+      devShell.x86_64-linux = hpkgs.shellFor {
+        packages = ps : [ ps."esqueleto-textsearch" ];
+        withHoogle = true;
+
+        buildInputs = [
+          hpkgs.haskell-language-server
+          pkgs.ghcid
+          pkgs.cabal-install
+        ];
+      };
+    };
+}
